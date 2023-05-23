928a9de802b7503b4f75578b512e1e5254c4223c
unknown <ddaamsnhau1999@gmail.com>
2023-05-24 01:18:13 +0700

    dang nhap demo one mail

--- test_view/test_navbar.js
+++ test_view/test_navbar.js
@@ -6,19 +6,14 @@ import puppeteer from "puppeteer";
     const page = await browser.newPage();
 
     // Điều hướng đến trang web cần kiểm tra
-    await page.goto('https://www.example.com');
+    await page.goto('https://webmail.onemail.vn/login');
+    await page.type('.login__input[autocomplete="username"]', 'demo@onemail.vn');
+    await page.type('.login__input[autocomplete="current-password"]', 'iNET@#2020');
+
 
     // Kiểm tra xem có phần tử navbar có tồn tại hay không
-    const navbarExists = await page.evaluate(() => {
-        const navbar = document.querySelector('nav');
-        return navbar !== null;
-    });
+    await page.click('.btn.btn-primary[type="submit"]');
 
-    if (navbarExists) {
-        console.log('Navbar tồn tại.');
-    } else {
-        console.log('Navbar không tồn tại.');
-    }
 
-    await browser.close();
+    // await browser.close();
 })();
